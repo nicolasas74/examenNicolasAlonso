00ea72fdb6de191f9caac5dd87f71682496ab79c
nicolasas74 <nicolasas74@educastur.es>
2024-03-18 13:04:02 +0100

    Añadidos comentarios de documentación en claseExamen

--- examenNicolasAlonso/src/main/java/miexamen/claseexamen.java
+++ examenNicolasAlonso/src/main/java/miexamen/claseexamen.java
@@ -1,8 +1,24 @@
+/**
+ * Entornos de desarrollo UT3
+ */
 package miexamen;
+/**
+ * @author :Nicolás Alonso Salas
+ * Versión : 1.0.0
+ * Date :18/03/2024
+ */
 
 public class claseexamen {
 
     public static void main(String[] args) {
         System.out.println("Estoy haciendo el examen de la UT3");
     }
+    /**
+     * Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de Desarrollo
+     */
+    
+    /**
+     * Curso de Git y github de píldoras informáticas.
+     * https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU
+     */
 }
